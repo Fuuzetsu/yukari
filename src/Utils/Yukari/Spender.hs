15736bdf7bb898d8bfc0b008032d86adc80901b4
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-11-07 20:10:25 +0000

    Space this out a bit

--- src/Utils/Yukari/Spender.hs
+++ src/Utils/Yukari/Spender.hs
@@ -22,5 +22,12 @@ spendYen regularSettings ys = do
   where attachBase yp = yp { spendingLinks = map (\(c, l) -> (,) c (baseSite regularSettings ++ "/" ++ l)) (spendingLinks yp)}
 
 chooseOptimal :: SpendSettings -> YenPage -> Maybe (Cost, String)
-chooseOptimal ys yp = let l = filterUnwanted ys yp in if null l then Nothing else Just $ foldl (\x@(c, q) y@(d, w) -> if c >= d then x else y) (head l) (tail l)
-  where filterUnwanted st pg = filter (\(c, _) -> yenOwned pg - c >= 0 && yenOwned pg - c >= yenLeftOver st) (spendingLinks pg)
+chooseOptimal ys yp = let l = filterUnwanted ys yp
+                      in case l of
+                        [] -> Nothing
+                        -- [(c, s)] -> Just (c, s)
+                        _ -> Just $ foldl1 (\x@(c, q) y@(d, w) -> if c >= d
+                                                                  then x
+                                                                  else y) l
+  where
+    filterUnwanted st pg = filter (\(c, _) -> yenOwned pg - c >= 0 && yenOwned pg - c >= yenLeftOver st) (spendingLinks pg)
