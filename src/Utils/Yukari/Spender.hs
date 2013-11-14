62913be12602ba38a607fc1d86b1fb950e2a62eb
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-11-14 02:16:33 +0000

    80C in Spender

--- src/Utils/Yukari/Spender.hs
+++ src/Utils/Yukari/Spender.hs
@@ -1,5 +1,6 @@
 module Utils.Yukari.Spender (spendYen) where
 
+import Control.Arrow (second)
 import Data.List (intersperse)
 import Utils.Yukari.Settings
 import Utils.Yukari.Crawler (getSinglePage)
@@ -13,7 +14,8 @@ spendYen ys = do
   let rs = siteSettings ys
   page <- parseYenPage =<< getSinglePage rs (yenSite $ spendSettings ys)
   verbPrint Low ys ["Currently have", show $ yenOwned page, "yen."]
-  verbPrint High ys ["Leftover yen limit set to", show . yenLeftOver $ spendSettings ys]
+  verbPrint High ys ["Leftover yen limit set to"
+                    , show . yenLeftOver $ spendSettings ys]
 
   case chooseOptimal (spendSettings ys) (attachBase page) of
     Nothing -> verbPrint Low ys ["No viable spending option found"]
@@ -22,17 +24,25 @@ spendYen ys = do
       verbPrint High ys ["Spending the yen at", link]
       getSinglePage rs link >> spendYen ys
   where
+    -- Attach the partial links we get from the site with
+    -- the full link of the base site, adding the ‘/’ separator
     attachBase :: YenPage -> YenPage
-    attachBase yp = yp { spendingLinks = map (\(c, l) -> (,) c (baseSite (siteSettings ys) ++ "/" ++ l)) (spendingLinks yp)}
+    attachBase yp =
+      let bs = baseSite (siteSettings ys) ++ "/"
+      in yp { spendingLinks = map (second (bs ++)) (spendingLinks yp) }
 
 
 chooseOptimal :: SpendSettings -> YenPage -> Maybe (Cost, String)
 chooseOptimal ys yp = let l = filterUnwanted ys yp
                       in case l of
                         [] -> Nothing
-                        -- [(c, s)] -> Just (c, s)
                         _ -> Just $ foldl1 (\x@(c, q) y@(d, w) -> if c >= d
                                                                   then x
                                                                   else y) l
   where
-    filterUnwanted st pg = filter (\(c, _) -> yenOwned pg - c >= 0 && yenOwned pg - c >= yenLeftOver st) (spendingLinks pg)
+    filterUnwanted :: SpendSettings -> YenPage -> [(Cost, String)]
+    filterUnwanted st pg =
+      let yOwned' = yenOwned pg
+          yenLeft' = yenLeftOver st
+          links = spendingLinks pg
+      in filter (\(c, _) -> yOwned' - c >= 0 && yOwned' - c >= yenLeft') links
