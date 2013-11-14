44ce406436f5602503a35a09c88b84ca7f4490c3
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-11-14 04:36:04 +0000

    Show what we're doing

--- src/Utils/Yukari.hs
+++ src/Utils/Yukari.hs
@@ -2,22 +2,20 @@
 module Utils.Yukari where
 
 import qualified Config.Dyre as Dyre
-import           Config.Dyre.Relaunch
-import           Control.Applicative
 import           Control.Monad
 import           System.Directory
 import           System.Environment
 import           System.Exit
 import           System.FilePath
 import           Utils.Yukari.Crawler
-import           Utils.Yukari.Filters
+import           Utils.Yukari.Formatter
 import           Utils.Yukari.Settings
 import           Utils.Yukari.Spender
-import           Utils.Yukari.Types
 
 showError :: Maybe YukariSettings -> String -> Maybe YukariSettings
 showError = const
 
+dps :: Dyre.Params (Maybe YukariSettings)
 dps = Dyre.defaultParams
   { Dyre.projectName = "Yukari"
   , Dyre.realMain = realMain
@@ -27,17 +25,24 @@ dps = Dyre.defaultParams
                                return $ hd </> ".yukari"
   }
 
+dyreParams :: Maybe YukariSettings -> IO ()
 dyreParams = Dyre.wrapMain dps
 
 realMain :: Maybe YukariSettings -> IO ()
 realMain Nothing = do
   putStrLn "Please set up your ~/.yukari/Yukari.hs"
   exitFailure
-realMain (Just ys@(YukariSettings site spend ps v)) = do
+realMain (Just ys@(YukariSettings _ _ ps _)) = do
   args <- getArgs
-  when (length args /= 0) $ do
+  unless (null args) $ do
     putStrLn $ "This program currently takes no arguments. "
       ++ "Populate your ~/.yukari/Yukari.hs instead"
     exitFailure
-  when (SpendYen `elem` ps) (spendYen ys)
-  when (DownloadTorrents `elem` ps) (crawlFromURL ys)
+
+  when (SpendYen `elem` ps) $ do
+    verbPrint Low ys ["Spending yen"]
+    spendYen ys
+
+  when (DownloadTorrents `elem` ps) $ do
+    verbPrint Low ys ["Dowloading torrents"]
+    crawlFromURL ys
